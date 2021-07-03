a56a4bd85d22b720af326ee5ba498eac75342e6c
srijan-27 <srastogi751@gmail.com>
2021-07-03 19:24:44 +0530

    Commit

---
+++ js/custom.js
@@ -0,0 +1,19 @@
+$('#yellow-color').click(function(e){
+  $(".main-wrapper-resume").attr("id", "yellow");
+});
+$('#red-color').click(function(e){
+  $(".main-wrapper-resume").attr("id", "red");
+});
+$('#blue-color').click(function(e){
+  $(".main-wrapper-resume").attr("id", "blue");
+});
+$('#green-color').click(function(e){
+  $(".main-wrapper-resume").attr("id", "green");
+});
+		 
+$(document).ready(function(){
+  $(".setting-icon").click(function(){
+    $(".color-box").toggleClass("main");
+  });
+});
+
